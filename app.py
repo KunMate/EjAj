98510d258fc76e0d2fe09d8f9e82e87a847abcb0
KunMate <79099087+KunMate@users.noreply.github.com>
2022-11-18 10:09:39 +0100

    11.18 / 10:09 update (completely working app)

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 import math
 import random
 
-
-def get_total_distance(route, object_f):
-    sum = 0
-    for a in range(len(route)):
-        sum += object_f(route[a])
-    return sum
+global n
+global n_drivers
 
 
-def generate_city(cities, demand):
-    loc_x = random.randint(1, 5000)
-    loc_y = random.randint(1, 5000)
-    cities.append((loc_x, loc_y))
-    demand.append(random.randint(1, 5))
+def calc_total_distance(drivers_route, object_f):
+    distance = 0
+    for d in drivers_route:
+        distance += object_f(d)
+    return distance
 
 
-def get_city_distances(base, cities):
+def generate_city(n):
+    demand = [0]
+    city_map = [(random.randint(1, 5000), random.randint(1, 5000))]  # bázis
+    for i in range(1, n + 1):
+        loc_x = random.randint(1, 5000)
+        loc_y = random.randint(1, 5000)
+        city_map.append((loc_x, loc_y))
+        demand.append(random.randint(1, 5))
+    return city_map, demand
+
+
+def calc_city_distances(base, cities):
     cdist = []
-    for j in range(len(cities)):
-        cdist.append(distance(base, cities[j]))
+    for city in cities:
+        cdist.append(distance(base, city))
     return cdist
 
 
@@ -35,136 +42,158 @@ def transform_tsp(tsp):
     return tsp_dict
 
 
-def object_function(dict, s):
+def object_function(distances_dict, s):
     dist = 0
     prev = s[0]
     for i in s:
-        dist += dict[(prev, i)]
+        dist += distances_dict[(prev, i)]
         prev = i
-    dist += dict[(s[-1], s[0])]
+    dist += distances_dict[(s[-1], s[0])]
     return dist
 
 
 def get_initial_route(cities, n, capacity, visited, demand):
-    solution = [0]
-    cdist = get_city_distances(cities[0], cities)
-    for i in range(n):
-        closestindex = 9999
-        distance = 2000000
+    solution = [0]  # inicializalja a bazissal
+    cdist = calc_city_distances(cities[0], cities)
+    for _ in range(n):
+        closest_index = math.inf
+        distance = math.inf
         for j in range(len(cdist)):
             if cdist[j] < distance and cdist[j] != 0 and j not in visited and demand[j] <= capacity:
-                closestindex = j
+                closest_index = j
                 distance = cdist[j]
-        if closestindex == 9999:
-            solution.append(0)
-            return solution
+        if closest_index == math.inf:
+            if capacity != 0 and len(visited) != len(cities):
+                capacity += 1
+            else:
+                solution.append(0)  # utolso lepeskent hozzateszi a bazis utat
+                return solution
         else:
-            solution.append(closestindex)
-            visited.append(closestindex)
-            capacity -= demand[closestindex]
-            cdist = get_city_distances(cities[closestindex], cities)
+            solution.append(closest_index)
+            visited.append(closest_index)
+            capacity -= demand[closest_index]
+            cdist = calc_city_distances(cities[closest_index], cities)
+    return None
 
 
-def search(route, object_f, iterations, demand, tabulist, tabulength):
-    am = list()
-    layers = 0
+def search(route, object_f, demand, tabulist, tabulength):
+    am = list()  # alternativ (nem jobb, de legkozelebb best-hez allo megoldas) csere adatait tarolo lista
+    n_of_alternates = 0
     counter = 0
+    retries = 3
     route_base = list(route)
-    fa = get_total_distance(route, object_f)
-    for _ in range(1000):
-        e = random.randint(0, len(route) - 1)
-        q = random.randint(0, len(route) - 1)
-        if e != q:
-            s1_base = list(route_base[e])
-            s2_base = list(route_base[q])
-            for _ in range(iterations):
-                a = random.randint(1, len(s1_base) - 2)
-                b = random.randint(1, len(s2_base) - 2)
-                if demand[s1_base[a]] != demand[s2_base[b]]:
-                    continue
-                s1_base[a], s2_base[b] = s2_base[b], s1_base[a]
-                route_base[e] = s1_base
-                route_base[q] = s2_base
-                fa_neighbor = get_total_distance(route_base, object_f)
-                if (fa - fa_neighbor) > 0 and s1_base not in tabulist and s2_base not in tabulist:
-                    print("Found better solution")
+    best_dist = calc_total_distance(route, object_f)
+    while True:
+        e = random.randint(0, len(route_base) - 1)
+        q = random.randint(0, len(route_base) - 1)
+        if e == q:
+            continue
+        s1_base = list(route_base[e])
+        s2_base = list(route_base[q])
+        a = random.randint(1, len(s1_base) - 2)
+        b = random.randint(1, len(s2_base) - 2)
+        if demand[s1_base[a]] != demand[s2_base[b]]:
+            continue
+        s1_base[a], s2_base[b] = s2_base[b], s1_base[a]
+        route_base[e] = s1_base
+        route_base[q] = s2_base
+        swapped_dist = calc_total_distance(route_base, object_f)
+        if best_dist > swapped_dist and s1_base not in tabulist and s2_base not in tabulist:
+            print("Found better solution")
+            counter = 0
+            n_of_alternates = 0
+            retries = 5
+            tabulist.insert(0, route_base[e])
+            tabulist.insert(0, route_base[q])
+            if len(tabulist) > tabulength:
+                tabulist.pop(len(tabulist) - 1)
+                tabulist.pop(len(tabulist) - 1)
+            route = list(route_base)
+            best_dist = swapped_dist
+            print("new:", best_dist)
+            if len(am) != 0:
+                am.pop(0)
+        else:
+            if len(am) == 0 and s1_base not in tabulist and s2_base not in tabulist:
+                am.append([a, b, e, q, swapped_dist])
+            elif len(am) != 0 and am[0][4] > swapped_dist and s1_base not in tabulist \
+                    and s2_base not in tabulist:
+                am.insert(0, [a, b, e, q, swapped_dist])
+                am.pop(1)
+            s1_tried = s1_base
+            s2_tried = s2_base
+            s1_base[a], s2_base[b] = s2_base[b], s1_base[a]
+            route_base[e] = s1_base
+            route_base[q] = s2_base
+            counter += 1
+            if counter == 50000:
+                n_of_alternates += 1
+                if n_of_alternates == 3:
+                    print("too many base swaps, going back to best")
+                    route_base = list(route)
+                    #best_dist = calc_total_distance(route, object_f)
+                    #print(best_dist)
+                    #swapped_dist = calc_total_distance(route_base, object_f)
+                    #print(swapped_dist)
+                    print("original:", best_dist)
+                    n_of_alternates = 0
                     counter = 0
-                    layers = 0
-                    tabulist.insert(0, route_base[e])
-                    tabulist.insert(0, route_base[q])
+                    retries -= 1
+                    if retries == 0:
+                        return route
+                else:
+                    print("using alternate route because I cant find a better route")
+                    route_base[am[0][2]][am[0][0]], route_base[am[0][3]][am[0][1]] = \
+                        route_base[am[0][3]][am[0][1]], route_base[am[0][2]][am[0][0]]
+                    am.pop(0)
+                    swapped_dist = calc_total_distance(route_base, object_f)
+                    tabulist.insert(0, s1_tried)
+                    tabulist.insert(0, s2_tried)
                     if len(tabulist) > tabulength:
                         tabulist.pop(len(tabulist) - 1)
                         tabulist.pop(len(tabulist) - 1)
-                    route = list(route_base)
-                    fa = get_total_distance(route, object_f)
-                    print("new:", fa)
-                    if len(am) != 0:
-                        am.pop(0)
-                else:
-                    counter += 1
-                    s1_base[a], s2_base[b] = s2_base[b], s1_base[a]
-                    if len(am) == 0:
-                        am.append([a, b, e, q, fa_neighbor])
-                    elif am[0][4] > fa_neighbor:
-                        am.insert(0, [a, b, e, q, fa_neighbor])
-                        am.pop(1)
-                    if counter == 50000:
-                        print("using alternate route because I cant find a better route")
-                        layers += 1
-                        route_base[am[0][2]][am[0][0]], route_base[am[0][3]][am[0][1]] =\
-                            route_base[am[0][3]][am[0][1]], route_base[am[0][2]][am[0][0]]
-                        counter = 0
-                        fa_neighbor = get_total_distance(route_base, object_f)
-                        print("new base:", fa_neighbor)
-                        am.pop(0)
-                        if layers == 3:
-                            print("too many layers, going back to best")
-                            route_base = list(route)
-                            fa = get_total_distance(route_base, object_f)
-                            print("original:", fa)
-                            layers = 0
-    return route
+                    counter = 0
+                    print("new base:", swapped_dist)
 
 
-def summarize(drivers, route, object_f):
-    summ = 0
-    for r in range(drivers):
-        summ += object_f(route[r])
-    for b in range(drivers):
+
+def summarize(n_drivers, route, object_f):
+    distance = 0
+    for r in range(n_drivers):
+        distance += object_f(route[r])
+    for b in range(n_drivers):
         print("route for driver", b, ":", route[b])
         print("distance for driver", b, ":", object_f(route[b]), "m")
-    print("total distance:", summ, "m")
+    print("total distance:", distance, "m")
 
 
 def main():
+    n = 500
+    n_drivers = 10
+    tabulength = 10
     random.seed(35142)
     object_f = lambda sched: object_function(tsp_dict, sched)
-    n = 100
     tabulist = []
-    tabulength = 6
-    cities = [(random.randint(1, 5000), random.randint(1, 5000))]  # bázis
-    demand = [0]
     visited = [0]
-    drivers = 10
     driver_capacities = []
     route = []
-    for i in range(1, n + 1):
-        generate_city(cities, demand)
+    city_map, demand = generate_city(n)
     demandsum = sum(demand)
-    tsp_dict = transform_tsp(cities)
-    for b in range(drivers):
-        if demandsum % drivers != 0:
+    tsp_dict = transform_tsp(city_map)
+    for b in range(n_drivers):
+        if demandsum % n_drivers != 0:
             if b == 0:
-                driver_capacities.append(math.floor(demandsum / drivers) + demandsum % drivers)
-                route.append(get_initial_route(cities, n, driver_capacities[b], visited, demand))
+                driver_capacities.append(math.floor(demandsum / n_drivers) + demandsum % n_drivers)
+                route.append(get_initial_route(city_map, n, driver_capacities[b], visited, demand))
             else:
-                driver_capacities.append(math.floor(demandsum / drivers))
-                route.append(get_initial_route(cities, n, driver_capacities[b], visited, demand))
+                driver_capacities.append(math.floor(demandsum / n_drivers))
+                route.append(get_initial_route(city_map, n, driver_capacities[b], visited, demand))
         else:
-            driver_capacities.append(math.floor(demandsum / drivers))
-            route.append(get_initial_route(cities, n, driver_capacities[b], visited, demand))
-    summarize(drivers, route, object_f)
-    route = search(route, object_f, 5000, demand, tabulist, tabulength)
-    summarize(drivers, route, object_f)
+            driver_capacities.append(math.floor(demandsum / n_drivers))
+            route.append(get_initial_route(city_map, n, driver_capacities[b], visited, demand))
+    summarize(n_drivers, route, object_f)
+    route = search(route, object_f, demand, tabulist, tabulength)
+    summarize(n_drivers, route, object_f)
+
 
 main()
